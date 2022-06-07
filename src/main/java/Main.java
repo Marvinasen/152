64f8d39a2a0be0c09d5801370ceb7ae7f0aaa2ef
Marvinasen <105413505+Marvinasen@users.noreply.github.com>
2022-06-07 11:11:22 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)  {
+        // Değişkenleri oluştur
+        int mat, fizik, kimya, turkce, tarih, muzik;
+
+        // Scanner sınıfınızı tanımladık
+        Scanner inp = new Scanner(System.in);
+
+        //Kullanıcıdan değerleri al
+        System.out.print("Matematik notunuz:");
+         mat = inp.nextInt();
+
+        System.out.print("Fizik notunuz:");
+        fizik = inp.nextInt();
+
+        System.out.print("Kimya notunuz:");
+        kimya = inp.nextInt();
+
+        System.out.print("Turkce notunuz:");
+        turkce = inp.nextInt();
+
+        System.out.print("Tarih notunuz:");
+        tarih = inp.nextInt();
+
+        System.out.print("Muzik notunuz:");
+        muzik = inp.nextInt();
+        int toplam = (mat + fizik + kimya + turkce + tarih + muzik);
+        double sonuc = (toplam / 6.0);
+        System.out.println("Ortalamaniz:" + sonuc);
+
+        boolean input = (sonuc > 59.999);
+        System.out.println("gecti");
+
+
+    }
+}
